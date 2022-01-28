44de5a8b3bb500fe24e9b2e7369f6ac1dcd89cfd
David García <wlogz@hotmail.com>
2022-01-27 22:51:54 -0500

    Creando rutas en Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateEmpleadosComponent } from './components/create-empleados/create-empleados.component';
+import { ListEmpleadosComponent } from './components/list-empleados/list-empleados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list-empleados', pathMatch: 'full' }, //Ordena a cargar el componente list-empleados
+  { path: 'list-empleados', component: ListEmpleadosComponent },
+  { path: 'create-empleados', component: CreateEmpleadosComponent },
+
+
+  { path: '**', redirectTo: 'list-empleados', pathMatch: 'full' }, //WildCard -> Si la dirección web tiene errores redirecciona al componente
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
